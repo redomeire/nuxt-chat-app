6e3b9b068945e139674cda04c2d595a70eea06fa
Redomeire <redomeire2@gmail.com>
2024-01-05 22:42:00 +0700

    create chat bubble

---
+++ components/chat/room/bubble.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+    defineProps({
+        text: String,
+        time: String
+    })
+</script>
+
+<template>
+    <div class="rounded-xl w-fit rounded-bl-none p-2 bg-white z-40 relative min-w-[100px] max-w-[200px] transition duration-200 translate-y-10">
+        <p class="text-sm font-semibold">{{ text }}</p>
+        <p class="text-[10px]">{{ time }}</p>
+    </div>
+</template>
--- components/chat/room/index.vue
+++ components/chat/room/index.vue
@@ -1,31 +1,73 @@
 <script setup lang="ts">
+import type { FormSubmitEvent } from '@nuxt/ui/dist/runtime/types';
+
 const state = reactive({
     value: ""
 })
+
+const messages = reactive([
+    {
+        text: 'Halo ges',
+        time: '13.00'
+    },
+    {
+        text: 'Iya halo',
+        time: '14.00'
+    }
+])
+
+const form = ref<HTMLFormElement>()
+
+const onSubmit = (event: FormSubmitEvent<any>) => {
+    messages.push({ text: state.value, time: '10.00' })
+    form.value?.reset()
+
+}
 </script>
 
 <template>
     <section class="chat-room w-2/3 h-screen">
         <ChatRoomNav />
         <div class="p-3 h-[90%] bg-[url('~/assets/images/doodle.jpg')] bg-center bg-contain relative">
-            <UForm :state="state" class="absolute bottom-10 left-0 right-0 flex items-stretch justify-between px-5 z-50 gap-3">
+            <div class="chat-bubble-container relative h-[90%] overflow-auto flex flex-col gap-3">
+                <div v-for="message of messages">
+                    <ChatRoomBubble :text="message.text" :time="message.time"/>
+                </div>
+            </div>
+            <UForm 
+                @submit="onSubmit"
+                :state="state"
+                ref="form"
+                class="absolute bottom-0 left-0 right-0 flex items-center justify-between px-5 z-50 gap-3 bg-white h-[10%]">
                 <UFormGroup name="value" class="w-full">
-                    <UInput size="lg" type="text" v-model="state.value" placeholder="Enter your message..."
+                    <UInput 
+                        size="lg" 
+                        type="text" 
+                        class="pr-20"
+                        variant="none"
+                        v-model="state.value" 
+                        placeholder="Enter your message..."
                         :ui="{ icon: { trailing: { pointer: '' } } }">
                         <template #trailing>
                             <div>
-                                <UButton icon="i-material-symbols-emoji-nature" color="gray" variant="link"
+                                <UButton type="button" icon="i-material-symbols-emoji-nature" color="gray" variant="link"
                                     class="text-gray-500 dark:text-gray-400 text-xs"></UButton>
-                                <UButton icon="i-material-symbols-attach-file" color="gray" variant="link"
+                                <UButton type="button" icon="i-material-symbols-attach-file" color="gray" variant="link"
                                     class="text-gray-500 dark:text-gray-400 text-xs"></UButton>
-                                <UButton icon="i-material-symbols-mic-rounded" color="gray" variant="link"
+                                <UButton type="button" icon="i-material-symbols-mic-rounded" color="gray" variant="link"
                                     class="text-gray-500 dark:text-gray-400 text-xs"></UButton>
                             </div>
                         </template>
                     </UInput>
                 </UFormGroup>
-                <UButton class="flex items-center justify-center" variant="solid" color="primary"
-                    icon="i-material-symbols-send"></UButton>
+                <UButton 
+                    type="submit" 
+                    class="flex items-center justify-center" 
+                    size="lg" 
+                    variant="solid" 
+                    color="primary"
+                    icon="i-material-symbols-send"
+                />
             </UForm>
         </div>
     </section>
